137aef42ce83ef5e9f002ea1da71b43e9241e680
Lesya Gaskova <antarcticacries@yandex.ru>
2022-11-22 23:37:18 +0300

    Update index.js
    Первый модуль, задача №3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,9 @@
 function ucFirst(str) {
-  // ваш код...
-}
+  if (str === '') {
+    return str;
+  };
+  const firstChar = str[0].toUpperCase();
+  const slicedString = str.slice(1);
+  const res = firstChar + slicedString;
+  return res;
+};

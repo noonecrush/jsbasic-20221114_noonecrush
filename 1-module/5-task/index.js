d0dcd1dd22a1a6120ac8c522ed9de1220da8a68a
Lesya Gaskova <antarcticacries@yandex.ru>
2022-11-23 15:11:43 +0300

    Первый модуль, задачи №2, 5
    Первый модуль, задачи №2, 5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,8 @@
 function truncate(str, maxlength) {
-  // ваш код...
-}
+  if (str.length <= maxlength) {
+    return str;
+  } else { 
+    const slicedStr = str.slice(0, maxlength - 1);
+    return slicedStr + '…';
+  }
+};

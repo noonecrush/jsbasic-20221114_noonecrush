e11dc519f4c9fe6014e6d2eecfe35d89e9121daa
Lesya Gaskova <antarcticacries@yandex.ru>
2022-11-23 15:18:11 +0300

    Правки по ревью задачи №3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -2,8 +2,5 @@ function ucFirst(str) {
   if (str === '') {
     return str;
   };
-  const firstChar = str[0].toUpperCase();
-  const slicedString = str.slice(1);
-  const res = firstChar + slicedString;
-  return res;
+  return str[0].toUpperCase() + str.slice(1);
 };

9db4d4206b46c5912295b5e3e474beadef9026ab
Lesya Gaskova <antarcticacries@yandex.ru>
2022-11-26 21:33:09 +0300

    Update 5 task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,4 +1,4 @@
 function checkSpam(str) {
   const strLowerCase = str.toLowerCase();
   return strLowerCase.includes('1xbet') || strLowerCase.includes('xxx');
-}
+};
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -2,7 +2,6 @@ function truncate(str, maxlength) {
   if (str.length <= maxlength) {
     return str;
   } else { 
-    const slicedStr = str.slice(0, maxlength - 1);
-    return slicedStr + '…';
+    return slicedStr = str.slice(0, maxlength - 1) + '…';
   }
 };

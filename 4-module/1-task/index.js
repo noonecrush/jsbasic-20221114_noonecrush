ecdd4d6e7f3a4912d47b308194189435d1e12679
Lesya Gaskova <antarcticacries@yandex.ru>
2022-12-14 22:25:13 +0300

    Update index.js

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let ulElement = document.createElement('ul');
+  friends.forEach((item) => {
+    let li =  document.createElement('li');
+    li.innerHTML = `${item.firstName} ${item.lastName}`;
+    ulElement.append(li);
+  });
+  return ulElement;
+};
